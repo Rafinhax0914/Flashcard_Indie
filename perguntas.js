7fcc77c99d7a32fa63e46153de14462a85734c4d
Rafinhax0914 <rafael.franco.anjos@escola.pr.gov.br>
2025-06-12 18:14:15 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Stardew Valley',
+    'Qual é o nome do criador de Stardew Valley?',
+    'ConcernedApe (Eric Barone).'
+)
+
+criaCartao(
+    'Hollow Knight',
+    'Qual é o nome do reino onde Hollow Knight se passa?',
+    'Hallownest.'
+)
+
+criaCartao(
+    'Undertale',
+    'Qual é o nome do esqueleto mais brincalhão em Undertale?',
+    'Sans.'
+)
+
+criaCartao(
+    'Celeste',
+    'Qual é o nome da montanha que Madeline tenta escalar?',
+    'Montanha Celeste (Celeste Mountain).'
+)
+
+criaCartao(
+    'Cuphead',
+    'Qual é o estilo artístico predominante em Cuphead?',
+    'Animação retrô dos anos 1930.'
+)
+
+criaCartao(
+    'Journey',
+    'Em Journey, qual é o principal objetivo do jogador?',
+    'Alcançar o topo da montanha brilhante.'
+)
